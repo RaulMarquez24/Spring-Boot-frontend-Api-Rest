4a6dfdafa796c1bf1708fbcd1113db0545f0dfcb
Raúl Márquez <raulmu785@gmail.com>
2023-10-31 13:11:11 +0100

    peticion al back

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from '../clientes/clientes.json';
 import { Cliente } from '../clientes/cliente';
-import { Observable, of  } from 'rxjs'
+import { Observable, of  } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
-
-  constructor() { }
+  private urlEndpoint:string = 'http://localhost:8081/api/clientes';
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    // return of(CLIENTES);
+    return this.http.get(this.urlEndpoint).pipe(
+      map( response => response as Cliente[] )
+    );
   }
 }
